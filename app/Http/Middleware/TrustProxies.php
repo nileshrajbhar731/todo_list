2091f464f450ad9c5f3351a30fecfc6cb8198589
Nilesh Bhardwaj <94599253+nileshrajbhar731@users.noreply.github.com>
2022-03-23 10:52:20 +0530

    Update TrustProxies.php
    update for testing

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,24 +2,24 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * @var array<int, string>|string|null
+     * @var array
      */
     protected $proxies;
 
     /**
-     * The headers that should be used to detect proxies.
+     * The current proxy header mappings.
      *
-     * @var int
+     * @var array
      */
-   protected $headers = [
+    protected $headers = [
         Request::HEADER_FORWARDED => 'FORWARDED',
         Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
         Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
